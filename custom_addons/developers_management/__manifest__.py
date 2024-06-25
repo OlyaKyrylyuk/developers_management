a147da23a333eb27be38cb6d91acb9c0738819ab
OlyaKyrylyuk <olichkakyrylyuk@gmail.com>
2024-06-26 00:03:16 +0300

    feat: added user access

--- custom_addons/developers_management/__manifest__.py
+++ custom_addons/developers_management/__manifest__.py
@@ -5,6 +5,7 @@
     'summary': 'Developers management module',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
         'views/developer_views.xml',
         'views/company_views.xml',
         'views/menu.xml',
